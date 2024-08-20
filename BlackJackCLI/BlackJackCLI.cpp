04e98a8183a9775dd8aed8f13b14537c96520038
cgafner <jj1gafner@gmail.com>
2024-08-19 20:17:21 -0500

    Adding first iteration of busting

--- BlackJackCLI/BlackJackCLI.cpp
+++ BlackJackCLI/BlackJackCLI.cpp
@@ -15,6 +15,12 @@ enum Suit {
     HEARTS, DIAMONDS, CLUBS, SPADES
 };
 
+/* Card Class
+ * This class is responsible for creating a card with a Rank and Suit.
+ * Any given card only knows about its own rank and suit.
+ * getRank(): returns the rank of the card.
+ * getSuit(): returns the suit of the card.
+ */
 class Card {
 public:
     Card(Rank rank, Suit suit) : rank_(rank), suit_(suit) {}
@@ -27,6 +33,15 @@ private:
     Suit suit_;
 };
 
+
+/* Deck Class
+ * This class is responsible for creating a deck with all 52 cards.
+ * Will make all ranks for each suit until all cards are made.
+ * Will add each card to a vector as a unique_ptr, for memory safety.
+ * shuffledeck(): This method will sort the cards randomly.
+ * drawCard(): Will draw a card from the deck if possible and return it.
+ * cards_: A private vector containing all cards in the Deck
+ */
 class Deck {
 public:
     Deck() {
@@ -56,8 +71,19 @@ private:
     vector<unique_ptr<Card> > cards_;
 };
 
+
+/* Player Class
+ * This class is responsible for keeping track of a player's cards and their total hand value.
+ * addCard(): This method will add a card from the Deck.
+ * getHandValue(): This method calculates the total hand value of the player.
+ * hand_: A private vector that contains all cards in the player's hand.
+ */
 class Player {
 public:
+    Player(string name)
+    {
+        name_ = name;
+    }
     void addCard(unique_ptr<Card> card) {
         hand_.push_back(std::move(card));
     }
@@ -81,28 +107,45 @@ public:
             value -= 10;
             aces--;
         }
+
+        if (value > 21) {
+            value = -1;
+        }
+
         return value;
+    }
+
+    void printHandValue(int value) const
+    {
+        cout << name_ << " hand value: " << value << '\n';
     }
 
 private:
     vector<unique_ptr<Card> > hand_;
+    string name_;
 };
 
 int main() {
     Deck deck;
     deck.shuffledeck();
 
-    Player player;
-    Player dealer;
+    Player player("player");
+    Player dealer("dealer");
 
-    player.addCard(deck.drawCard());
-    player.addCard(deck.drawCard());
+    while(player.getHandValue() != -1 || dealer.getHandValue() != -1)
+    {
+    	player.addCard(deck.drawCard());
+    	player.addCard(deck.drawCard());
+        player.printHandValue(player.getHandValue());
 
-    dealer.addCard(deck.drawCard());
-    dealer.addCard(deck.drawCard());
+    	dealer.addCard(deck.drawCard());
+    	dealer.addCard(deck.drawCard());
+        dealer.printHandValue(dealer.getHandValue());
 
-    cout << "Player hand value: " << player.getHandValue() << endl;
-    cout << "Dealer hand value: " << dealer.getHandValue() << endl;
+        cout << "\nNext deal." << "\n\n";
+    }
+
+    cout << "\n\nGame over.";
 
     return 0;
 }
